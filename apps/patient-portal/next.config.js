86585b67e2540503795eb86b2837813edb3c0d68
Imasha Weerakoon <imasha98.we@gmail.com>
2025-02-25 21:27:53 +0530

    feat: add next-intl plugin for internationalization support

--- apps/patient-portal/next.config.js
+++ apps/patient-portal/next.config.js
@@ -1,7 +1,9 @@
 //@ts-check
 
- 
 const { composePlugins, withNx } = require('@nx/next');
+const createNextIntlPlugin = require('next-intl/plugin');
+
+const withNextIntl = createNextIntlPlugin();
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -17,6 +19,7 @@ const nextConfig = {
 const plugins = [
   // Add more Next.js plugins to this list if needed.
   withNx,
+  withNextIntl,
 ];
 
 module.exports = composePlugins(...plugins)(nextConfig);
