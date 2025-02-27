5962e16dc46c4206e82ec3b14e733c76a04bb980
Imasha Weerakoon <imasha98.we@gmail.com>
2025-02-27 20:44:03 +0530

    feat: wrap RootLayout in ThemeProvider for theme integration

--- apps/patient-portal/src/app/layout.tsx
+++ apps/patient-portal/src/app/layout.tsx
@@ -1,6 +1,5 @@
-import '@wexelcode/theme';
-
 import { DashboardLayout } from '@wexelcode/layouts';
+import { ThemeProvider } from '@wexelcode/theme';
 
 import { UserMenu } from '../user-menu';
 
@@ -15,12 +14,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>
-        <DashboardLayout navigationItems={[]} userMenu={<UserMenu />}>
-          {children}
-        </DashboardLayout>
-      </body>
-    </html>
+    <ThemeProvider>
+      <html lang="en">
+        <body>
+          <DashboardLayout navigationItems={[]} userMenu={<UserMenu />}>
+            {children}
+          </DashboardLayout>
+        </body>
+      </html>
+    </ThemeProvider>
   );
 }
