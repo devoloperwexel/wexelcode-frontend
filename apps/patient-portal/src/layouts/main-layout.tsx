29722285217a5694a4ba31b52354b4da2bc4a66b
Imasha Weerakoon <imasha98.we@gmail.com>
2025-02-28 23:39:10 +0530

    fix: adjust main layout height for better overflow handling

--- apps/patient-portal/src/layouts/main-layout.tsx
+++ apps/patient-portal/src/layouts/main-layout.tsx
@@ -6,8 +6,8 @@ export default function MainLayout({ children }: PropsWithChildren) {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
       <Header />
-      <main className="grid grid-cols-7 h-full">
-        <div className="col-start-2 col-span-5 p-4 h-full">{children}</div>
+      <main className="grid grid-cols-7 h-[calc(100vh-4rem)] overflow-auto">
+        <div className="col-start-2 col-span-5 p-4 h-full ">{children}</div>
       </main>
     </div>
   );
