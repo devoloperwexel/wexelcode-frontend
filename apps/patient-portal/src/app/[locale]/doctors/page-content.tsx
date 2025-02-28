fe476cc7dd9fc31461b2f4d4b35e20dc91b21d7e
Imasha Weerakoon <imasha98.we@gmail.com>
2025-02-28 23:38:47 +0530

    feat: enhance DoctorsPageContent with loading state and skeleton UI

--- apps/patient-portal/src/app/[locale]/doctors/page-content.tsx
+++ apps/patient-portal/src/app/[locale]/doctors/page-content.tsx
@@ -3,28 +3,37 @@
 import { DatePicker, Input, Pagination } from '@wexelcode/components';
 import { useGetDoctors, useQueryParams } from '@wexelcode/hooks';
 
-import { DoctorCard } from '../../../components/doctors';
+import {
+  DoctorCard,
+  DoctorsLoadingSkeleton,
+} from '../../../components/doctors';
 
 export default function DoctorsPageContent() {
   const queryParams = useQueryParams();
 
-  const { data: response } = useGetDoctors({
+  const { data: response, isLoading } = useGetDoctors({
     page: queryParams.getInt('page') || 1,
     limit: queryParams.getInt('limit') || 1,
     includes: ['user'],
   });
 
   return (
-    <div className="space-y-6">
-      <div className="grid grid-cols-4 gap-4">
-        <Input placeholder="Search Doctors" className="col-span-3" />
-        <DatePicker initialDate={new Date()} />
-      </div>
+    <div className="flex flex-col justify-between space-y-4 h-full">
+      <div className="space-y-6">
+        <div className="grid grid-cols-4 gap-4">
+          <Input placeholder="Search Doctors" className="col-span-3" />
+          <DatePicker initialDate={new Date()} />
+        </div>
 
-      <div className="grid grid-cols-4 gap-4">
-        {response?.data?.results.map((item) => (
-          <DoctorCard key={item.id} doctor={item} />
-        ))}
+        <div className="grid grid-cols-4 gap-4">
+          {isLoading ? (
+            <DoctorsLoadingSkeleton />
+          ) : (
+            response?.data?.results.map((item) => (
+              <DoctorCard key={item.id} doctor={item} />
+            ))
+          )}
+        </div>
       </div>
 
       <Pagination totalPages={response?.data?.totalPages || 0} />
