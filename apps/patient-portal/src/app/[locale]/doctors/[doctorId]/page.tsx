7b54a4a364df84b26c8c5224d9ed563c330037ca
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-01 22:44:31 +0530

    refactor: remove DoctorPageContent and related components for cleaner structure

--- apps/patient-portal/src/app/[locale]/doctors/[doctorId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import DoctorPageContent from './page-content';
-
-interface DoctorPageProps {
-  doctorId: string;
-}
-
-export default function DoctorPage({ doctorId }: DoctorPageProps) {
-  return <DoctorPageContent />;
-}
