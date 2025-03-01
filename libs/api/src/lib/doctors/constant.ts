d519e024b8be0fbab6d2b9a63aa612e74f987764
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-01 10:39:44 +0530

    feat: add API endpoints and functions for fetching doctor by ID and availability

--- libs/api/src/lib/doctors/constant.ts
+++ libs/api/src/lib/doctors/constant.ts
@@ -3,6 +3,14 @@ const API = {
     path: '/physios',
     method: 'GET',
   },
+  GET_BY_ID: {
+    path: '/physios/:id',
+    method: 'GET',
+  },
+  GET_AVAILABILITY: {
+    path: '/physios/:id/availability',
+    method: 'GET',
+  },
 };
 
 export default API;
--- libs/api/src/lib/doctors/doctors.ts
+++ libs/api/src/lib/doctors/doctors.ts
@@ -1,12 +1,40 @@
-import { GetDoctorsRequest, GetDoctorsResponse } from '@wexelcode/types';
+import {
+  GetDoctorAvailabilityRequest,
+  GetDoctorResponse,
+  GetDoctorsAvailabilityResponse,
+  GetDoctorsRequest,
+  GetPaginatedDoctorsResponse,
+} from '@wexelcode/types';
 import { request } from '@wexelcode/utils';
 
 import API from './constant';
 
 export const GetDoctors = async (params: GetDoctorsRequest) => {
-  const response = await request<GetDoctorsResponse>(
+  const response = await request<GetPaginatedDoctorsResponse>(
     API.GET_ALL,
-    {},
+    null,
+    {
+      params,
+    }
+  );
+
+  return response;
+};
+
+export const GetDoctorById = async (id: string) => {
+  const response = await request<GetDoctorResponse>(API.GET_BY_ID, null, {
+    params: { id },
+  });
+
+  return response;
+};
+
+export const GetDoctorAvailability = async (
+  params: GetDoctorAvailabilityRequest
+) => {
+  const response = await request<GetDoctorsAvailabilityResponse>(
+    API.GET_AVAILABILITY,
+    null,
     {
       params,
     }
