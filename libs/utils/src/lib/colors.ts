ef694e262e529f01201822af2a240541dcc82587
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-05 00:39:05 +0530

    feat: add utility function to generate color from title string

---
+++ libs/utils/src/lib/colors.ts
@@ -0,0 +1,16 @@
+export const titleToColor = (title: string) => {
+  // Simple hash function to convert a string to a number
+  let hash = 0;
+  for (let i = 0; i < title.length; i++) {
+    hash = title.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  // Convert the hash to a 6-digit hexadecimal color code
+  let color = '#';
+  for (let i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += ('00' + value.toString(16)).substr(-2);
+  }
+
+  return color;
+};
