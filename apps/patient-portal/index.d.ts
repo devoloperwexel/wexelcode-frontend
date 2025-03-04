8fd22d90122b1fe7eb6a91add0ec82d796fd4696
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-05 00:37:31 +0530

    feat: extend next-auth session to include custom user type

--- apps/patient-portal/index.d.ts
+++ apps/patient-portal/index.d.ts
@@ -1,6 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { CustomAdapterUser } from '@wexelcode/types';
 declare module '*.svg' {
   const content: any;
   export const ReactComponent: any;
   export default content;
+}
+
+declare module 'next-auth' {
+  interface Session {
+    user: CustomAdapterUser;
+  }
 }
