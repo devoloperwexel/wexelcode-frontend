daa880a8526fe58fda660d7d4d03bc6e082b8be3
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-07 00:32:18 +0530

    feat: extend type definitions for next-auth session and axios request config

--- apps/doctor-portal/index.d.ts
+++ apps/doctor-portal/index.d.ts
@@ -1,6 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { CustomAdapterUser } from '@wexelcode/types';
 declare module '*.svg' {
   const content: any;
   export const ReactComponent: any;
   export default content;
+}
+
+declare module 'next-auth' {
+  interface Session {
+    user: CustomAdapterUser;
+  }
+}
+
+declare module 'axios' {
+  export interface AxiosRequestConfig {
+    authenticated?: boolean;
+  }
 }
