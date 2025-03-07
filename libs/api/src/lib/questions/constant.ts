76a1533d8ce4773d852d223360046ca0337595fd
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-08 00:20:25 +0530

    feat: update API constants for retrieving all questionnaires and questions by questionnaire ID

--- libs/api/src/lib/questions/constant.ts
+++ libs/api/src/lib/questions/constant.ts
@@ -1,6 +1,10 @@
 const API = {
-  GET_ALL: {
-    path: '/Questionnaires',
+  GET_ALL_QUESTIONNAIRES: {
+    path: '/questionnaires',
+    method: 'GET',
+  },
+  GET_QUESTIONS_BY_QUESTIONNAIRE_ID: {
+    path: 'questionnaires/550e8400-e29b-41d4-a716-446655440001/questions?page=1&limit=20',
     method: 'GET',
   },
 };
