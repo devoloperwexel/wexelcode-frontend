dff27530c7bc2fe426a23e1970e8422bb5b71e0c
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-08 00:18:50 +0530

    feat: add GetQuestionsResponse type for enhanced questionnaire response handling

--- libs/types/src/lib/questions/questions-response.types.ts
+++ libs/types/src/lib/questions/questions-response.types.ts
@@ -1,4 +1,5 @@
 import { PaginatedResponse } from '../..';
-import { Questionnaire } from '.';
+import { Question, Questionnaire } from '.';
 
 export type GetQuestionnaireResponse = PaginatedResponse<Questionnaire>;
+export type GetQuestionsResponse = PaginatedResponse<Question>;
