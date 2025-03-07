8682b07fbd16e2fdaae842db7f28a46c17ed6239
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-08 00:19:03 +0530

    feat: update GetQuestionnaire API to fetch all questionnaires and add GetQuestionnaireByQuestionnaireId for specific questionnaire retrieval

--- libs/api/src/lib/questions/questions.ts
+++ libs/api/src/lib/questions/questions.ts
@@ -1,15 +1,31 @@
 import {
   GetQuestionnaireResponse,
   GetQuestionsRequest,
+  GetQuestionsResponse,
 } from '@wexelcode/types';
 import { request } from '@wexelcode/utils';
 
 import API from './constant';
 
 export const GetQuestionnaire = async (params: GetQuestionsRequest) => {
-  const response = await request<GetQuestionnaireResponse>(API.GET_ALL, null, {
-    params,
-  });
+  const response = await request<GetQuestionnaireResponse>(
+    API.GET_ALL_QUESTIONNAIRES,
+    null,
+    {
+      params,
+    }
+  );
+
+  return response?.data;
+};
+
+export const GetQuestionnaireByQuestionnaireId = async (id: string) => {
+  const response = await request<GetQuestionsResponse>(
+    API.GET_QUESTIONS_BY_QUESTIONNAIRE_ID,
+    {
+      id,
+    }
+  );
 
   return response?.data;
 };
