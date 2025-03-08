6d594335f91659927081d63eae7a54dc8f4deb98
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-08 22:02:36 +0530

    feat: update GET_QUESTIONS_BY_QUESTIONNAIRE_ID path to use dynamic questionId parameter

--- libs/api/src/lib/questions/constant.ts
+++ libs/api/src/lib/questions/constant.ts
@@ -4,7 +4,7 @@ const API = {
     method: 'GET',
   },
   GET_QUESTIONS_BY_QUESTIONNAIRE_ID: {
-    path: 'questionnaires/550e8400-e29b-41d4-a716-446655440001/questions?page=1&limit=20',
+    path: 'questionnaires/:questionId/questions',
     method: 'GET',
   },
 };
