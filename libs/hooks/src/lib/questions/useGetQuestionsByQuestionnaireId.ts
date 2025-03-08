0d45ebc863ea4e2d3ecfbe6306aaa824cd805527
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-08 22:01:23 +0530

    feat: refactor useGetQuestionsByQuestionnaireId to accept parameters object for improved flexibility

--- libs/hooks/src/lib/questions/useGetQuestionsByQuestionnaireId.ts
+++ libs/hooks/src/lib/questions/useGetQuestionsByQuestionnaireId.ts
@@ -1,10 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
 import { GetQuestionnaireByQuestionnaireId } from '@wexelcode/api';
 import { QueryKeys } from '@wexelcode/constants';
+import { GetQuestionnaireByQuestionnaireIdRequest } from '@wexelcode/types';
 
-export const useGetQuestionsByQuestionnaireId = (id: string) => {
+export const useGetQuestionsByQuestionnaireId = (
+  params: GetQuestionnaireByQuestionnaireIdRequest
+) => {
   return useQuery({
-    queryKey: [QueryKeys.questionnaire, id],
-    queryFn: async () => GetQuestionnaireByQuestionnaireId(id),
+    queryKey: [
+      QueryKeys.questionnaire,
+      params.questionId,
+      params.page,
+      params.limit,
+    ],
+    queryFn: async () => GetQuestionnaireByQuestionnaireId(params),
   });
 };
