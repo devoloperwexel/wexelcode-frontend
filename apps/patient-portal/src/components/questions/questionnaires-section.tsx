65d1d6b120f79c34255f86030aec98355d40403e
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-10 23:25:02 +0530

    feat: enhance QuestionnaireSection to accept local and gender props for improved question handling

--- apps/patient-portal/src/components/questions/questionnaires-section.tsx
+++ apps/patient-portal/src/components/questions/questionnaires-section.tsx
@@ -1,22 +1,29 @@
 'use client';
 
-import { Tabs, TabsList, TabsTrigger } from '@wexelcode/components';
 import { useGetQuestionnaire } from '@wexelcode/hooks';
-import { ClipboardListIcon } from 'lucide-react';
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 
 import QuestionForm from './questions-form';
 import QuestionsNavigationMenu from './questions-navigation-menu';
 
-export function QuestionnaireSection() {
+interface QuestionnaireSectionProps {
+  local: string;
+  gender: string;
+}
+
+export function QuestionnaireSection({
+  local,
+  gender,
+}: QuestionnaireSectionProps) {
   const [currentCategoryIndex, setCurrentCategoryIndex] = useState(0);
+
   const { data: questionnaires } = useGetQuestionnaire({
     page: 1,
     limit: 20,
   });
 
   return (
-    <div className="bg-white rounded-lg shadow-lg overflow-hidden transition-all duration-200">
+    <div className="bg-white rounded-lg shadow-lg transition-all duration-200">
       {questionnaires?.results && (
         <QuestionsNavigationMenu
           questionnaires={questionnaires.results}
@@ -28,6 +35,11 @@ export function QuestionnaireSection() {
         {questionnaires?.results && (
           <QuestionForm
             questionnaire={questionnaires.results[currentCategoryIndex]}
+            index={currentCategoryIndex}
+            total={questionnaires.results.length}
+            local={local}
+            gender={gender}
+            onChangeIndex={setCurrentCategoryIndex}
           />
         )}
       </div>
