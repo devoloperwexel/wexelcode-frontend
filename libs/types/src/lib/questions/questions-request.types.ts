868a08628b91e030dca59e38b0352b3ef6118520
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-10 23:23:47 +0530

    feat: add optional requiredQuestionId to GetQuestionnaireByQuestionnaireIdRequest interface

--- libs/types/src/lib/questions/questions-request.types.ts
+++ libs/types/src/lib/questions/questions-request.types.ts
@@ -4,4 +4,5 @@ export type GetQuestionnaireRequest = BaseRequest;
 
 export interface GetQuestionnaireByQuestionnaireIdRequest extends BaseRequest {
   id: string;
+  requiredQuestionId?: string;
 }
