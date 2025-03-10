988428044b1dab1705c7b6041812af41527f50e4
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-10 19:46:36 +0530

    fix: correct class name for overflow handling in main layout

--- apps/patient-portal/src/layouts/main-layout.tsx
+++ apps/patient-portal/src/layouts/main-layout.tsx
@@ -6,7 +6,7 @@ export default function MainLayout({ children }: PropsWithChildren) {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
       <Header />
-      <main className="grid grid-cols-7 h-[calc(100vh-4rem)] overflow-auto">
+      <main className="grid grid-cols-7 h-[calc(100vh-4rem)] y-overflow-auto">
         <div className="col-start-2 col-span-5 p-4 h-full ">{children}</div>
       </main>
     </div>
