49236d25b59f232d946e6468bcbf30046d1fab36
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-12 22:24:00 +0530

    feat: add GET_APPOINTMENT_BY_ID constant for fetching appointment details by ID

--- libs/api/src/lib/appointments/constant.ts
+++ libs/api/src/lib/appointments/constant.ts
@@ -3,6 +3,10 @@ const API = {
     path: 'user/:userId/appointments',
     method: 'POST',
   },
+  GET_APPOINTMENT_BY_ID: {
+    path: 'user/:userId/appointments/:appointmentId',
+    method: 'GET',
+  },
 };
 
 export default API;
