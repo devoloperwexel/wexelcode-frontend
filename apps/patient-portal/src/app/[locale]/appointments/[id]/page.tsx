be89130fe861d7b69b65ad894ae78950bae4e0b2
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-12 22:24:35 +0530

    feat: add TODO for SSR fetch in AppointmentDetailsPage

--- apps/patient-portal/src/app/[locale]/appointments/[id]/page.tsx
+++ apps/patient-portal/src/app/[locale]/appointments/[id]/page.tsx
@@ -10,6 +10,6 @@ export default async function AppointmentDetailsPage({
   params,
 }: AppointmentDetailsPageProps) {
   const { id } = await params;
-
+  // TODO: SSR Fetch
   return <AppointmentDetailsPageContent id={id} />;
 }
