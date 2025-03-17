4a48fec1e75aa212e1be9cc87410e71fe5f238dd
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-17 22:54:33 +0530

    feat: enhance DetailsReview component with internationalization and improved layout

--- apps/patient-portal/src/components/details/details-review.tsx
+++ apps/patient-portal/src/components/details/details-review.tsx
@@ -1,61 +1,72 @@
+import { Text } from '@wexelcode/components';
+import { useTranslations } from 'next-intl';
 import { useFormContext } from 'react-hook-form';
 
 export function DetailsReview() {
+  const t = useTranslations('profile');
   const { getValues } = useFormContext();
 
   return (
-    <div className="space-y-6">
-      <h2 className="text-xl font-semibold">Review Your Information</h2>
+    <>
       <div className="bg-gray-50 p-4 rounded-md">
-        <h3 className="font-medium mb-2">Personal Details</h3>
+        <Text variant="h4" className="mb-2">
+          {t('completeProfilePage.personalDetails')}
+        </Text>
+
         <div className="grid grid-cols-2 gap-4">
-          <p>
-            <span className="font-medium">Birthday:</span>
-            {getValues('birthday')}
-          </p>
-          <p>
-            <span className="font-medium">Gender:</span>
-            {getValues('gender')}
-          </p>
-          <p>
-            <span className="font-medium">Address:</span> {getValues('gender')}
-          </p>
-          <p>
-            <span className="font-medium">City:</span> {getValues('gender')}
-          </p>
-          <p>
-            <span className="font-medium">County:</span> {getValues('gender')}
-          </p>
-          <p>
-            <span className="font-medium">Zipcode:</span> {getValues('gender')}
-          </p>
-          {/* <p className="col-span-2">
-              <span className="font-medium">Languages:</span>{' '}
-              {formData.languages.join(', ')}
-            </p> */}
+          <Text>
+            {t('personalDetailsForm.birthday')}:
+            <span className="font-medium ml-2">{getValues('birthDay')}</span>
+          </Text>
+          <Text>
+            {t('personalDetailsForm.address')}:
+            <span className="font-medium ml-2">{getValues('address')}</span>
+          </Text>
+          <Text>
+            {t('personalDetailsForm.city')}:
+            <span className="font-medium ml-2">{getValues('city')}</span>
+          </Text>
+          <Text>
+            {t('personalDetailsForm.zipCode')}:
+            <span className="font-medium ml-2">{getValues('zipCode')}</span>
+          </Text>
+          <Text>
+            {t('personalDetailsForm.languages')}:
+            <span className="font-medium ml-2">
+              {getValues('languages').join(', ')}
+            </span>
+          </Text>
         </div>
       </div>
+
       <div className="bg-gray-50 p-4 rounded-md">
-        <h3 className="font-medium mb-2">Medical Details</h3>
+        <Text variant="h4" className="mb-2">
+          {t('completeProfilePage.medicalDetails')}
+        </Text>
+
         <div className="grid grid-cols-2 gap-4">
-          <p>
-            <span className="font-medium">Occupation:</span>{' '}
-            {getValues('gender')}
-          </p>
-          <p>
-            <span className="font-medium">Weight:</span> {getValues('gender')}{' '}
-            kg
-          </p>
-          <p>
-            <span className="font-medium">Height:</span> {getValues('gender')}
-            cm
-          </p>
-          <p className="col-span-2">
-            <span className="font-medium">Activities:</span>{' '}
-            {/* {formData.activities.join(', ') || 'None'} */}
-          </p>
+          <Text>
+            {t('medicalDetailsForm.occupation')}:
+            <span className="font-medium ml-2">{getValues('occupation')}</span>
+          </Text>
+          <Text>
+            {t('medicalDetailsForm.weight')}:
+            <span className="font-medium ml-2">{getValues('weight')}</span>
+          </Text>
+          <Text>
+            {t('medicalDetailsForm.height')}:
+            <span className="font-medium ml-2">{getValues('height')}</span>
+          </Text>
+          {getValues('activities') && (
+            <Text className="col-span-2">
+              {t('medicalDetailsForm.activities')}:
+              <span className="font-medium ml-2">
+                {getValues('activities').join(', ')}
+              </span>
+            </Text>
+          )}
         </div>
       </div>
-    </div>
+    </>
   );
 }
