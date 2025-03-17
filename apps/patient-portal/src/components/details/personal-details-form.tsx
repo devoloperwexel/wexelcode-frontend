fb01f9a63469c0e3f7fbb8a791a11a850dcaf064
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-17 22:55:05 +0530

    feat: enhance PersonalDetailsForm with internationalization and multi-select for languages

--- apps/patient-portal/src/components/details/personal-details-form.tsx
+++ apps/patient-portal/src/components/details/personal-details-form.tsx
@@ -1,41 +1,69 @@
 import {
   FormDateInputField,
   FormInputField,
+  FormMultiSelectField,
   FormSelectField,
 } from '@wexelcode/components';
+import { useTranslations } from 'next-intl';
 
 export function PersonalDetailsForm() {
+  const t = useTranslations('profile.personalDetailsForm');
+
+  const genderOptions = [
+    {
+      label: 'Male',
+      value: 'Male',
+    },
+    {
+      label: 'Female',
+      value: 'Female',
+    },
+  ];
+
+  const languagesOptions = [
+    {
+      label: 'English',
+      value: 'English',
+    },
+    {
+      label: 'Spanish',
+      value: 'Spanish',
+    },
+    {
+      label: 'French',
+      value: 'French',
+    },
+  ];
+
   return (
     <div className="space-y-4">
-      <h1>Personal Details Form</h1>
-
       <div className="grid grid-cols-2 gap-6">
-        <FormDateInputField label="Date of Birth" name="dateOfBirth" />
+        <FormInputField label={t('birthday')} name="birthDay" type="date" />
 
         <FormSelectField
-          label="Gender"
+          label={t('gender')}
           name="gender"
-          options={[
-            {
-              label: 'Male',
-              value: 'male',
-            },
-            {
-              label: 'Female',
-              value: 'female',
-            },
-          ]}
+          options={genderOptions}
         />
       </div>
 
       <div>
-        <FormInputField label="Address" name="address" />
+        <FormInputField label={t('address')} name="address" />
       </div>
 
       <div className="grid grid-cols-3 gap-6">
-        <FormInputField label="City" name="city" />
-        <FormInputField label="Country" name="county" />
-        <FormInputField label="Zipcode" name="zipcode" />
+        <FormInputField label={t('city')} name="city" />
+        <FormInputField label={t('state')} name="country" />
+        <FormInputField label={t('zipCode')} name="zipCode" type="number" />
+      </div>
+
+      <div>
+        <FormMultiSelectField
+          label={t('languages')}
+          name="languages"
+          options={languagesOptions}
+          placeholder={t('selectLanguages')}
+        />
       </div>
     </div>
   );
