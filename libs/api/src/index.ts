4496bdf2b9be27383fc99677dfde19fbbb27c35c
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-18 00:25:12 +0530

    feat: export patient module from API index

--- libs/api/src/index.ts
+++ libs/api/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/appointments/appointments';
 export * from './lib/doctors/doctors';
+export * from './lib/patients/patients';
 export * from './lib/questions/questions';
 export * from './lib/users/users';
