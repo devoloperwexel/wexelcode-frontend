4f2aced314127d9d3bcc26f5667a11ee1f1d9932
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-18 00:26:24 +0530

    feat: enable patient data prefetching in DetailsPage component

--- apps/patient-portal/src/app/[locale]/profile/complete/[userId]/page.tsx
+++ apps/patient-portal/src/app/[locale]/profile/complete/[userId]/page.tsx
@@ -14,10 +14,10 @@ export default async function DetailsPage({ params }: DetailsPageProps) {
   const { userId } = await params;
   const queryClient = new QueryClient();
 
-  // await queryClient.prefetchQuery({
-  //   queryKey: [QueryKeys.users, userId, QueryKeys.patients],
-  //   queryFn: async () => GetPatientsByUserId(userId),
-  // });
+  await queryClient.prefetchQuery({
+    queryKey: [QueryKeys.users, userId, QueryKeys.patients],
+    queryFn: async () => GetPatientsByUserId(userId),
+  });
 
   return <DetailsPageContent userId={userId} />;
 }
