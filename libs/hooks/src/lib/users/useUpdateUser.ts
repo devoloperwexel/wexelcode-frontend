0b32e66f476aa49498f8d4a4aa10842dbf6e667f
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-19 21:54:47 +0530

    feat: integrate session update on successful user mutation

--- libs/hooks/src/lib/users/useUpdateUser.ts
+++ libs/hooks/src/lib/users/useUpdateUser.ts
@@ -1,9 +1,15 @@
 import { useMutation } from '@tanstack/react-query';
 import { UpdateUser } from '@wexelcode/api';
 import { UpdateUserRequest } from '@wexelcode/types';
+import { useSession } from 'next-auth/react';
 
 export const useUpdateUser = () => {
+  const { update } = useSession();
+
   return useMutation({
     mutationFn: async (request: UpdateUserRequest) => await UpdateUser(request),
+    onSuccess: async (response) => {
+      await update(response?.data);
+    },
   });
 };
