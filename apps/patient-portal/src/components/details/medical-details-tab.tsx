efdaf392d56e790e8d5f71567895da765c11e034
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-19 23:14:53 +0530

    feat: enhance MedicalDetailsTab with patient data fetching and update functionality

--- apps/patient-portal/src/components/details/medical-details-tab.tsx
+++ apps/patient-portal/src/components/details/medical-details-tab.tsx
@@ -8,27 +8,37 @@ import {
   CardHeader,
   Form,
 } from '@wexelcode/components';
+import { useGetPatientByUserId, useUpdatePatient } from '@wexelcode/hooks';
+import { getDirtyValues } from '@wexelcode/utils';
+import { useSession } from 'next-auth/react';
+import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 
 import { MedicalDetailsForm } from './medical-details-form';
 
 export function MedicalDetailsTab() {
+  const { data: userData } = useSession();
+
+  const { data: patientData } = useGetPatientByUserId(userData?.user.id);
+
   const form = useForm();
 
-  // const { data } = useSession();
+  const { mutate: update } = useUpdatePatient();
 
-  // useEffect(() => {
-  //   form.reset(data?.user);
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [data]);
+  useEffect(() => {
+    form.reset(patientData);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [patientData]);
 
-  const handleSave = async (data: any) => {
-    console.log(data);
+  const handleNSave = async (data: any) => {
+    const dirtyValues = getDirtyValues(form.formState.dirtyFields, data);
+    // TODO: remove userID
+    update({ userId: userData?.user.id, id: patientData?.id, ...dirtyValues });
   };
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(handleSave)}>
+      <form onSubmit={form.handleSubmit(handleNSave)}>
         <Card>
           <CardHeader>Medical Details</CardHeader>
           <CardContent>
