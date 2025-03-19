f76e50b04cd58a8d7f3d4a4e3baed3ae53daa7f6
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-19 22:39:20 +0530

    refactor: simplify ProfilePageContent layout by removing unnecessary div wrapper

--- apps/patient-portal/src/app/[locale]/profile/page-content.tsx
+++ apps/patient-portal/src/app/[locale]/profile/page-content.tsx
@@ -14,21 +14,19 @@ import {
 
 export default function ProfilePageContent() {
   return (
-    <div className="max-w-3xl mx-auto">
-      <Tabs defaultValue="personal">
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="personal">Personal</TabsTrigger>
-          <TabsTrigger value="medical">Medical</TabsTrigger>
-        </TabsList>
+    <Tabs defaultValue="personal">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="personal">Personal</TabsTrigger>
+        <TabsTrigger value="medical">Medical</TabsTrigger>
+      </TabsList>
 
-        <TabsContent value="personal">
-          <PersonalDetailsTab />
-        </TabsContent>
+      <TabsContent value="personal">
+        <PersonalDetailsTab />
+      </TabsContent>
 
-        <TabsContent value="medical">
-          <MedicalDetailsTab />
-        </TabsContent>
-      </Tabs>
-    </div>
+      <TabsContent value="medical">
+        <MedicalDetailsTab />
+      </TabsContent>
+    </Tabs>
   );
 }
