41f2cdde2e91c55e5c6f41be2d7e632c9676fe04
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-19 23:16:06 +0530

    feat: enhance useUpdatePatient hook to invalidate patient queries on successful update

--- libs/hooks/src/lib/patients/useUpdatePatient.ts
+++ libs/hooks/src/lib/patients/useUpdatePatient.ts
@@ -1,10 +1,18 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { UpdatePatient } from '@wexelcode/api';
+import { QueryKeys } from '@wexelcode/constants';
 import { UpdatePatientRequest } from '@wexelcode/types';
 
 export const useUpdatePatient = () => {
+  const client = useQueryClient();
+
   return useMutation({
     mutationFn: async (request: UpdatePatientRequest) =>
       await UpdatePatient(request),
+    onSuccess: () => {
+      client.invalidateQueries({
+        queryKey: [QueryKeys.patients],
+      });
+    },
   });
 };
