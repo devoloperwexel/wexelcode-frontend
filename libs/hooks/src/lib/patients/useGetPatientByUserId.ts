fc875efbe97b24b3abb0b639b4cdd6ab13f9632b
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-19 23:15:39 +0530

    feat: update useGetPatientByUserId hook to handle optional user ID and enable query conditionally

--- libs/hooks/src/lib/patients/useGetPatientByUserId.ts
+++ libs/hooks/src/lib/patients/useGetPatientByUserId.ts
@@ -2,9 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 import { GetPatientsByUserId } from '@wexelcode/api';
 import { QueryKeys } from '@wexelcode/constants';
 
-export const useGetPatientByUserId = (id: string) => {
+export const useGetPatientByUserId = (id?: string) => {
   return useQuery({
     queryKey: [QueryKeys.users, id, QueryKeys.patients],
-    queryFn: async () => await GetPatientsByUserId(id),
+    queryFn: async () => await GetPatientsByUserId(id!),
+    enabled: !!id,
   });
 };
