c1cdd0f6275afc7c47469106f170be935f370c41
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-20 20:16:35 +0530

    feat: add ProfileCompleteProvider to redirect users to complete their profile if address is missing

--- apps/patient-portal/src/components/user-menu/user-menu.tsx
+++ apps/patient-portal/src/components/user-menu/user-menu.tsx
@@ -13,15 +13,13 @@ import {
 import { ChevronsUpDown, Cog, LogOut } from 'lucide-react';
 import { signIn, signOut, useSession } from 'next-auth/react';
 import { useTranslations } from 'next-intl';
-import { useEffect } from 'react';
 
 import Routes from '../../constants/routes';
-import { Link, useRouter } from '../../i18n/routing';
+import { Link } from '../../i18n/routing';
 
 export function UserMenu() {
   const t = useTranslations('userMenu');
   const { data, status } = useSession();
-  const { push } = useRouter();
 
   const handleSinIn = async () => {
     await signIn('keycloak', { redirectTo: window.location.href });
@@ -30,15 +28,6 @@ export function UserMenu() {
   const handleSinOut = async () => {
     await signOut({ redirect: false });
   };
-
-  useEffect(() => {
-    // TODO: Need Refactor
-    if (status === 'authenticated') {
-      if (!data.user.address) {
-        push(`${Routes.profile.complete}/${data.user.id}`);
-      }
-    }
-  }, [data, status]);
 
   if (status === 'loading')
     return (
