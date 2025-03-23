c8bd2227da847b0e31ce012c838530cf0ce4a255
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-23 16:58:55 +0530

    feat: add dateTimeDiff function for calculating date differences

--- libs/utils/src/lib/date-time-format.ts
+++ libs/utils/src/lib/date-time-format.ts
@@ -3,3 +3,7 @@ import moment from 'moment';
 export const dateTimeFormat = (date: string | Date, format: string) => {
   return moment(date).format(format);
 };
+
+export const dateTimeDiff = (first: string | Date, second: string | Date) => {
+  return moment(first).diff(moment(second));
+};
