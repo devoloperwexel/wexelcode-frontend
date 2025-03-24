1d3b8ab3f8b3e06c44c52525ed692df2efbc033b
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-24 23:33:24 +0530

    feat: enhance ScreeningResultCard with internationalization support

--- apps/patient-portal/src/components/dashboard/screening-result-card.tsx
+++ apps/patient-portal/src/components/dashboard/screening-result-card.tsx
@@ -12,10 +12,12 @@ import {
 import { useGetAnswers } from '@wexelcode/hooks';
 import { ActivityIcon, CalculatorIcon, CheckCircleIcon } from 'lucide-react';
 import { useSession } from 'next-auth/react';
+import { useTranslations } from 'next-intl';
 
 import { QuestionnaireDialog } from '../questions';
 
 export function ScreeningResultCard() {
+  const t = useTranslations('dashboard.screeningResultCard');
   const { data: userData } = useSession();
 
   const { data: response } = useGetAnswers({
@@ -27,16 +29,21 @@ export function ScreeningResultCard() {
   return (
     <Card>
       <Dialog>
-        <CardHeader>Latest Screening Result</CardHeader>
+        <CardHeader>{t('title')}</CardHeader>
         <CardContent className="space-y-4">
           <div className="flex flex-col items-center justify-start">
-            <ProgressIndicator percentage={score} />
+            <ProgressIndicator percentage={score} size={140}>
+              <div className="flex flex-col justify-center text-center">
+                <Text>{score} %</Text>
+                <Text variant="muted">{t('score')}</Text>
+              </div>
+            </ProgressIndicator>
           </div>
 
           <div className="flex justify-between">
             <div className="flex items-center space-x-2">
               <CalculatorIcon className="w-5 h-5 text-primary" />
-              <Text>Screening Date</Text>
+              <Text>{t('date')}</Text>
             </div>
             <Text variant="muted">March 23, 2025 </Text>
           </div>
@@ -44,7 +51,7 @@ export function ScreeningResultCard() {
           <div className="flex justify-between">
             <div className="flex items-center space-x-2">
               <ActivityIcon className="w-5 h-5 text-primary" />
-              <Text>Result Status</Text>
+              <Text>{t('result')}</Text>
             </div>
             <div className="flex items-center space-x-2">
               <CheckCircleIcon className="w-6 h-6 text-green-500" />
@@ -58,7 +65,7 @@ export function ScreeningResultCard() {
         <CardFooter className="w-full">
           <DialogTrigger asChild>
             <Button variant="outline" className="w-full">
-              Start New Screening
+              {t('startNew')}
             </Button>
           </DialogTrigger>
         </CardFooter>
