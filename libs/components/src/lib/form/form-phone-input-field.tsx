843303efb3e9b68e6ed7862a4aa0e0d385be1ec7
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-24 21:52:40 +0530

    feat: add FormPhoneInputField component for enhanced phone input functionality

---
+++ libs/components/src/lib/form/form-phone-input-field.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import { Input, PhoneInput, Textarea } from '../../';
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '.';
+
+type Props = Omit<React.ComponentProps<typeof FormField>, 'render'> &
+  React.ComponentProps<typeof PhoneInput> & {
+    label?: string;
+    placeholder?: string;
+    description?: string;
+  };
+
+const FormPhoneInputField = ({
+  label,
+  placeholder,
+  description,
+  ...rest
+}: Props) => {
+  return (
+    <FormField
+      {...rest}
+      render={({ field }) => {
+        return (
+          <FormItem>
+            {label && <FormLabel>{label}</FormLabel>}
+            <FormControl>
+              <PhoneInput {...field} placeholder={placeholder} {...rest} />
+            </FormControl>
+            {description && <FormDescription>{description}</FormDescription>}
+            <FormMessage />
+          </FormItem>
+        );
+      }}
+    />
+  );
+};
+
+FormPhoneInputField.displayName = 'FormPhoneInputField';
+
+export { FormPhoneInputField };
