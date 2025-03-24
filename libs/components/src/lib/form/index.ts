fe600021a702b31492e60d473195882c53012762
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-24 21:53:11 +0530

    feat: export FormPhoneInputField from form index for enhanced phone input functionality

--- libs/components/src/lib/form/index.ts
+++ libs/components/src/lib/form/index.ts
@@ -3,3 +3,4 @@ export * from './form-date-input-field';
 export * from './form-dropdown-multi-selector';
 export * from './form-dropdown-selector';
 export * from './form-input-field';
+export * from './form-phone-input-field';
