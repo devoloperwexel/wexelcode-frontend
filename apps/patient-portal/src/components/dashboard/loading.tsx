98fc17d870d21206f775ec959ee8fb990aa26608
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-25 20:11:30 +0530

    feat: enhance LoadingAppointmentCard with improved layout and additional Skeleton components

--- apps/patient-portal/src/components/dashboard/loading.tsx
+++ apps/patient-portal/src/components/dashboard/loading.tsx
@@ -1,22 +1,35 @@
-import { Card, CardContent, CardHeader, Skeleton } from '@wexelcode/components';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  Skeleton,
+} from '@wexelcode/components';
 
 export function LoadingAppointmentCard() {
   return (
-    <Card>
+    <Card className="flex flex-col">
       <CardHeader>
         <Skeleton className="w-3/4 h-4" />
       </CardHeader>
-      <CardContent className="flex flex-col space-y-2">
-        <div className="flex space-x-4">
-          <Skeleton className="w-15 h-15" />
-          <Skeleton className="w-1/3 h-4" />
+      <CardContent className="flex flex-col flex-grow space-y-2">
+        <div className="flex items-center space-x-4 ">
+          <Skeleton className="w-16 h-16 rounded-full" />
+          <div className="w-full space-y-2">
+            <Skeleton className="w-2/3 h-2" />
+            <Skeleton className="w-1/3 h-2" />
+          </div>
         </div>
-
-        <Skeleton className="w-1/2 h-2" />
-        <Skeleton className="w-1/3 h-2" />
-
-        <Skeleton className="w-full h-4" />
+        <div className="grid grid-cols-2 gap-4">
+          <Skeleton className="w-full h-10" />
+          <Skeleton className="w-full h-10" />
+          <Skeleton className="w-full h-10" />
+          <Skeleton className="w-full h-10" />
+        </div>
       </CardContent>
+      <CardFooter>
+        <Skeleton className="w-full h-4" />
+      </CardFooter>
     </Card>
   );
 }
