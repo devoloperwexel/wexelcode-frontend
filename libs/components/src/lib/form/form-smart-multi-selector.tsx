c6df2dec10736a2266bd7e571d75cc4e154b7566
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-28 00:48:54 +0530

    feat: add SmartMultiSelect and SmartSelect components for enhanced selection functionality

---
+++ libs/components/src/lib/form/form-smart-multi-selector.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import { SmartMultiSelect } from '../..';
+import {
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '.';
+
+type Props = Omit<React.ComponentProps<typeof FormField>, 'render'> &
+  Omit<React.ComponentProps<typeof SmartMultiSelect>, 'onChange' | 'value'> & {
+    label?: string;
+    description?: string;
+  };
+
+const FormSmartMultiSelectorField = ({
+  label,
+  placeholder,
+  emptyText,
+  searchPlaceholder,
+  description,
+  options = [],
+  ...rest
+}: Props) => {
+  return (
+    <FormField
+      {...rest}
+      render={({ field }) => (
+        <FormItem>
+          {label && <FormLabel>{label}</FormLabel>}
+
+          <SmartMultiSelect
+            options={options}
+            value={field.value}
+            onChange={field.onChange}
+            placeholder={placeholder}
+            emptyText={emptyText}
+            searchPlaceholder={searchPlaceholder}
+          />
+
+          {description && <FormDescription>{description}</FormDescription>}
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+FormSmartMultiSelectorField.displayName = 'FormSmartMultiSelectorField';
+
+export { FormSmartMultiSelectorField };
