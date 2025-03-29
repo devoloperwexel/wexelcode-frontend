633c208dc95d5ae895f2f83e09bff2899a253679
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-29 23:05:29 +0530

    feat: add 'use client' directive to multiple components for improved client-side rendering

--- apps/patient-portal/src/components/dashboard/latest-upcoming-appointment-card.tsx
+++ apps/patient-portal/src/components/dashboard/latest-upcoming-appointment-card.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { calenderIcon } from '@wexelcode/assets';
 import {
   Button,
--- libs/components/src/lib/animation/animation-on-scroll.tsx
+++ libs/components/src/lib/animation/animation-on-scroll.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { motion, useAnimation, useInView } from 'framer-motion';
 import React, { useEffect, useRef } from 'react';
 
