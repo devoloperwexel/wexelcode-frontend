47ff4f49b539ee278d7b77ed6b4dc3ae8b235e36
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-31 19:56:19 +0530

    refactor: change directory

--- apps/patient-portal/src/app/[locale]/appointments/page-content.tsx
+++ apps/patient-portal/src/app/[locale]/appointments/page-content.tsx
@@ -10,7 +10,7 @@ import { dateTimeSubtract } from '@wexelcode/utils';
 import { CalendarIcon, ClockIcon } from 'lucide-react';
 import { useTranslations } from 'next-intl';
 
-import { AppointmentListView } from '../../../../components/appointments/appointment-list-view';
+import { AppointmentListView } from '../../../components/appointments/appointment-list-view';
 
 export default function AppointmentsPageContent() {
   const t = useTranslations('appointments.AppointmentsPage');
