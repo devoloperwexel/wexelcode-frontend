627b3afc56b451844dcedc1ef0201e20d9938947
Imasha Weerakoon <imasha98.we@gmail.com>
2025-03-31 23:37:58 +0530

    feat: integrate AppointmentCalendarCard into dashboard layout

--- apps/patient-portal/src/app/[locale]/dashboard/page.tsx
+++ apps/patient-portal/src/app/[locale]/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { ProtectedVisible } from '../../../components/common';
 import {
+  AppointmentCalenderCard,
   LatestUpcomingAppointmentCard,
   ScreeningResultCard,
 } from '../../../components/dashboard';
@@ -11,6 +12,9 @@ export default function Dashboard() {
         <div className="grid grid-cols-2 gap-4">
           <ScreeningResultCard />
           <LatestUpcomingAppointmentCard now={new Date()} />
+          <div className="col-span-2">
+            <AppointmentCalenderCard />
+          </div>
         </div>
       </ProtectedVisible>
     </div>
