7d7b4d0e7eb0ee01710e436482bfdd721d0a09b4
Imasha Weerakoon <imasha98.we@gmail.com>
2025-04-01 23:17:04 +0530

    feat: remove language options from constants

--- libs/constants/src/lib/options.ts
+++ libs/constants/src/lib/options.ts
@@ -1,5 +1,3 @@
 export const GenderOptions = ['MALE', 'FEMALE'];
 
-export const LanguageOptions = ['ENGLISH', 'GERMAN', 'FRENCH', 'SPANISH'];
-
 export const ActivityOptions = ['FOOTBALL', 'BASKETBALL', 'SKI'];
