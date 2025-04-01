b653e2c53123fd6c6daa3cfe75d9cd5e21cb2cd3
Imasha Weerakoon <imasha98.we@gmail.com>
2025-04-01 20:06:01 +0530

    feat: update protected routes to support German language

--- apps/patient-portal/src/middleware.ts
+++ apps/patient-portal/src/middleware.ts
@@ -5,7 +5,7 @@ export default auth(async function middleware(request: NextRequest) {
   // Your custom middleware logic goes here
   const { pathname } = request.nextUrl;
   const protectedRoutesRegex =
-    /^\/(en|ar)\/(profile|appointments|dashboard)(\/.*)?$/;
+    /^\/(en|de)\/(profile|appointments|dashboard)(\/.*)?$/;
   const isProtectedRoute = protectedRoutesRegex.test(pathname);
 
   if (isProtectedRoute) {
