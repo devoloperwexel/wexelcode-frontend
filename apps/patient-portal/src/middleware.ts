f4cc8bb8afe5b156704878ffccc81d7e1da37fdc
Thakshara Dhananjaya <thakshara@surge.global>
2025-04-02 19:07:31 +0530

    change auth middleware

--- apps/patient-portal/src/middleware.ts
+++ apps/patient-portal/src/middleware.ts
@@ -6,28 +6,32 @@ import { routing } from './i18n/routing';
 
 const intlMiddleware = createMiddleware(routing);
 
-export default auth(async function middleware(request: NextRequest) {
-  // Your custom middleware logic goes here
+const authMiddleware = auth((request: NextRequest) => {
+  const { pathname } = request.nextUrl;
+  const host = process.env?.['NEXTAUTH_URL'];
+
+  if ((request as any).auth) {
+    return intlMiddleware(request);
+  } else {
+    const signInUrl = new URL('/api/auth/signin', host);
+    signInUrl.searchParams.set('callbackUrl', `${host}${pathname}`);
+
+    return NextResponse.redirect(signInUrl);
+  }
+});
+
+export default function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const protectedRoutesRegex =
     /^\/(en|de)\/(profile|appointments|dashboard)(\/.*)?$/;
   const isProtectedRoute = protectedRoutesRegex.test(pathname);
 
   if (isProtectedRoute) {
-    const host = process.env?.['NEXTAUTH_URL'];
-
-    if ((request as any).auth) {
-      return intlMiddleware(request);
-    } else {
-      const signInUrl = new URL('/api/auth/signin', host);
-      signInUrl.searchParams.set('callbackUrl', `${host}${pathname}`);
-
-      return NextResponse.redirect(signInUrl);
-    }
+    return (authMiddleware as any)(request);
   } else {
     return intlMiddleware(request);
   }
-});
+}
 
 // Match only internationalized pathnames
 export const config = {
