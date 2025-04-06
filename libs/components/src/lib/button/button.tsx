62ae5692214485b55671fba9c566d63681f68a4d
Thakshara Dhananjaya <thaksharadhananjaya@gmail.com>
2025-04-06 16:40:14 +0530

    add loading button

--- libs/components/src/lib/button/button.tsx
+++ libs/components/src/lib/button/button.tsx
@@ -1,6 +1,7 @@
 import { Slot } from '@radix-ui/react-slot';
 import { cn } from '@wexelcode/utils';
 import { cva, type VariantProps } from 'class-variance-authority';
+import { Loader2 } from 'lucide-react';
 import * as React from 'react';
 
 const buttonVariants = cva(
@@ -36,17 +37,32 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
+  loading?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  (
+    {
+      className,
+      variant,
+      size,
+      asChild = false,
+      loading = false,
+      children,
+      ...props
+    },
+    ref
+  ) => {
     const Comp = asChild ? Slot : 'button';
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
-      />
+      >
+        {loading && <Loader2 className="animate-spin" />}
+        {children}
+      </Comp>
     );
   }
 );
