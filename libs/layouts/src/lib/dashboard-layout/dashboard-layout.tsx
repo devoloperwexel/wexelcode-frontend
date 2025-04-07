125bb22ac438bba10ca0565c5d22b440ef7d895b
Imasha Weerakoon <imasha98.we@gmail.com>
2025-04-07 20:07:26 +0530

    fix: remove top padding from children container in DashboardLayout

--- libs/layouts/src/lib/dashboard-layout/dashboard-layout.tsx
+++ libs/layouts/src/lib/dashboard-layout/dashboard-layout.tsx
@@ -81,7 +81,7 @@ export function DashboardLayout({
 
           {actionComponent}
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
+        <div className="flex flex-1 flex-col gap-4 p-4">{children}</div>
       </SidebarInset>
     </SidebarProvider>
   );
