1d43694012e80a6837ac2eb8d8de642ae9da7328
Thakshara Dhananjaya <thaksharadhananjaya@gmail.com>
2025-04-08 15:09:23 +0530

    validate appoint success page

--- apps/patient-portal/src/app/[locale]/appointments/[id]/success/page.tsx
+++ apps/patient-portal/src/app/[locale]/appointments/[id]/success/page.tsx
@@ -1,5 +1,32 @@
+import { auth } from '@wexelcode/auth';
 import AppointmentSuccessPageContent from './page-content';
+import { GetAppointmentById } from '@wexelcode/api';
+import { notFound } from 'next/navigation';
 
-export default function AppointmentSuccessPage() {
-  return <AppointmentSuccessPageContent />;
+interface AppointmentSuccessPageProps {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default async function AppointmentSuccessPage({
+  params,
+}: AppointmentSuccessPageProps) {
+  const [paramsData, session] = await Promise.all([params, auth()]);
+  const { id } = paramsData;
+
+  try {
+    const appointment = await GetAppointmentById({
+      userId: session?.user?.id,
+      appointmentId: id,
+    });
+
+    if (appointment?.status !== 'SUCCESS') {
+      notFound();
+    }
+
+    return <AppointmentSuccessPageContent />;
+  } catch (_error) {
+    notFound();
+  }
 }
