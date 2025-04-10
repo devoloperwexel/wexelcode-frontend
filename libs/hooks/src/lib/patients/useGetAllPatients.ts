86928057b0f1af731e5327efaf5e1c393692f6f0
Imasha Weerakoon <imasha98.we@gmail.com>
2025-04-10 21:57:42 +0530

    feat: add useGetAllPatients hook for retrieving all patients

---
+++ libs/hooks/src/lib/patients/useGetAllPatients.ts
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query';
+import { GetAllPatients } from '@wexelcode/api';
+import { QueryKeys } from '@wexelcode/constants';
+import { GetAllPatientsRequest } from '@wexelcode/types';
+
+export const useGetAllPatients = (request: GetAllPatientsRequest) => {
+  return useQuery({
+    queryKey: [
+      QueryKeys.patients,
+      request.physioUserId,
+      request.limit,
+      request.page,
+    ],
+    queryFn: async () => await GetAllPatients(request),
+  });
+};
