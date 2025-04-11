bf86fefe93438083ff398fd8c66c6a766170cf3b
Thakshara Dhananjaya <thaksharadhananjaya@gmail.com>
2025-04-11 16:02:49 +0530

    install next top loader

--- apps/patient-portal/src/app/[locale]/layout.tsx
+++ apps/patient-portal/src/app/[locale]/layout.tsx
@@ -5,11 +5,10 @@ import { QueryProvider } from '@wexelcode/react-query';
 import { notFound } from 'next/navigation';
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
-
-import { LinkProgressBar } from '../../components/link-progress-bar/link-progress-bar';
 import MainLayout from '../../layouts/main-layout';
 import { ProfileCompleteProvider } from '../../providers';
 import { routing } from './../../i18n/routing';
+import NextTopLoader from 'nextjs-toploader';
 
 export const metadata = {
   title: 'WexelCode',
@@ -39,7 +38,11 @@ export default async function LocaleLayout({
     <AuthProvider>
       <html lang={locale}>
         <body>
-          <LinkProgressBar />
+          <NextTopLoader
+            color="#A51008"
+            speed={300}
+            showForHashAnchor={false}
+          />
           <QueryProvider>
             <NextIntlClientProvider messages={messages}>
               <ProfileCompleteProvider>
