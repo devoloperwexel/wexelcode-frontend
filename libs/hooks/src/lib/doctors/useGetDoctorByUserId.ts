fb54d27fe9a7c0bdd8c58586b5560d40fe3f093a
Imasha Weerakoon <imasha98.we@gmail.com>
2025-04-16 19:35:53 +0530

    feat: update useGetDoctorByUserId to accept optional id parameter

--- libs/hooks/src/lib/doctors/useGetDoctorByUserId.ts
+++ libs/hooks/src/lib/doctors/useGetDoctorByUserId.ts
@@ -2,9 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 import { GetDoctorByUserId } from '@wexelcode/api';
 import { QueryKeys } from '@wexelcode/constants';
 
-export const useGetDoctorByUserId = (id: string) => {
+export const useGetDoctorByUserId = (id?: string) => {
   return useQuery({
     queryKey: [QueryKeys.doctors, id],
-    queryFn: async () => GetDoctorByUserId(id),
+    queryFn: async () => GetDoctorByUserId(id!),
+    enabled: !!id, // TODO: remove this when we have a better solution for the user id
   });
 };
