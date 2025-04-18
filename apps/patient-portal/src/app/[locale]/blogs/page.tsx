a4bcfa3efb6eaeaea248e056c5bb7348a016bd60
Thakshara Dhananjaya <thaksharadhananjaya@gmail.com>
2025-04-18 14:59:44 +0530

    fix: lint issues

--- apps/patient-portal/src/app/[locale]/blogs/page.tsx
+++ apps/patient-portal/src/app/[locale]/blogs/page.tsx
@@ -1,6 +1,7 @@
-import Routes from 'apps/patient-portal/src/constants/routes';
-import { redirect } from 'apps/patient-portal/src/i18n/routing';
 import { getLocale } from 'next-intl/server';
+
+import Routes from '../../../constants/routes';
+import { redirect } from '../../../i18n/routing';
 
 export default async function BlogPage() {
   const locale = await getLocale();
