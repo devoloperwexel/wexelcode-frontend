6f9e71cdc67ef7fb80a36367de6e58ac566d7166
Thakshara Dhananjaya <thaksharadhananjaya@gmail.com>
2025-06-07 19:10:08 +0530

    fix: add optional chaining for user data in UserMenu and UserAvatar components

--- libs/components/src/lib/avatar/avatar.tsx
+++ libs/components/src/lib/avatar/avatar.tsx
@@ -66,6 +66,7 @@ const UserAvatar = ({ name, profileUrl, ...rest }: UserAvatarProps) => {
     const [firstName, lastName] = name.split(' ');
     return `${firstName[0]}${lastName ? lastName[0] : ''}`.toLocaleUpperCase();
   };
+console.log(name);
 
   return (
     <Avatar {...rest}>
